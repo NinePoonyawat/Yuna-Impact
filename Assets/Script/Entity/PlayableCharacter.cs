5e7a7b26d9b2236cfeef97e6bfb7b56ab81afe95
Nithimet Smart <89740267+KiraraGZ@users.noreply.github.com>
2023-03-09 21:35:15 +0700

    misc
    + temp character icon

--- Assets/Script/Entity/PlayableCharacter.cs
+++ Assets/Script/Entity/PlayableCharacter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayableCharacter : Entity
 {
     // Start is called before the first frame update
+    [Header ("Character")]
     [SerializeField] private CharacterProfile characterProfile;
     [SerializeField] private float attackRange;
     public int blockCount = 3;
