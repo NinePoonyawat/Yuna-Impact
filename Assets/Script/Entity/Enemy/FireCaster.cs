971489b1aa632e17f665c8696a9dae398114b818
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2023-03-11 21:38:03 +0700

    debug about fire mage

--- Assets/Script/Entity/Enemy/FireCaster.cs
+++ Assets/Script/Entity/Enemy/FireCaster.cs
@@ -7,14 +7,13 @@ public class FireCaster : Enemy
     [Header ("Fire Caster")]
     public GameObject fireAreaPf;
     public float explodeDuration;
-    public int explodeRadius;
+    public float explodeRadius;
 
     public override bool CallAttack(EntityController toAttack)
     {
         AfterAttack();
         GameObject GO = Instantiate(fireAreaPf,gameController.projectileRoot);
         GO.transform.position = toAttack.transform.position;
-        Debug.Log("enter");
         FireFloorCasting fireFloorCasting = GO.GetComponent<FireFloorCasting>();
         fireFloorCasting.SetUp(explodeDuration,explodeRadius,(EnemyController) entityController,this);
         entityController.SetEntityState(EntityState.PREATTACK);
