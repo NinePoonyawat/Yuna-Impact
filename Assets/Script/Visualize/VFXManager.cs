479080fdbdc26672985d4205cca2636c36130acd
Nithimet Smart <89740267+KiraraGZ@users.noreply.github.com>
2023-03-11 21:48:08 +0700

    VFX

--- Assets/Script/Visualize/VFXManager.cs
+++ Assets/Script/Visualize/VFXManager.cs
@@ -6,10 +6,13 @@ public class VFXManager : MonoBehaviour
 {
     [SerializeField] private GameObject effects;
     [SerializeField] private Transform point;
+    [SerializeField] private bool onThis;
 
     public void Play()
     {
         if (effects == null || transform == null) return;
-        Instantiate(effects, point.position, point.rotation);
+
+        if (onThis) Instantiate(effects, point.position, point.rotation, point);
+        else Instantiate(effects, point.position, point.rotation);
     }
 }
