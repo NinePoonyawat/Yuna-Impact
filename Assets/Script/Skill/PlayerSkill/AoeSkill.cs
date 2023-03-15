876895b1385854e307689f90f894d2037f0b8098
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2023-03-15 15:28:22 +0700

    all skill

--- Assets/Script/Skill/PlayerSkill/AoeSkill.cs
+++ Assets/Script/Skill/PlayerSkill/AoeSkill.cs
@@ -8,6 +8,9 @@ public class AoeSkill : PlayerSkill
     public int damage = 1;
     public float range = 1.5f;
     public AttackType damageType;
+
+    public GameObject statusPf;
+    public float float1;
     
     [Header ("Indicator")]
     public GameObject rangeIndicator;
@@ -16,12 +19,27 @@ public class AoeSkill : PlayerSkill
     {
         SetCooldown();
 
-        Debug.Log("YES");
-        List<EnemyController> characters = gameController.FindAllNearestEnemy(transform.position, range);
-        foreach(EnemyController target in characters)
+        Vector3 start = transform.position;
+        Collider[] colliders = Physics.OverlapCapsule(new Vector3(start.x,start.y + 1,start.z),new Vector3(start.x,start.y - 1,start.z),
+            range,mask);
+        foreach(var collider in colliders)
         {
-            Debug.Log(target);
-            target.TakeDamage(damage, damageType);
+            EnemyController enemyController = collider.GetComponent<EnemyController>();
+            if (enemyController != null)
+            {
+                enemyController.TakeDamage(damage, damageType);
+                if (statusPf != null)
+                {
+                    GameObject GO = Instantiate(statusPf,enemyController.transform);
+                    Status status = GO.GetComponent<Status>();
+                    switch (status.GetType().Name)
+                    {
+                        case ("StunStatus") :
+                            ((StunStatus) status).Setting(float1,enemyController);
+                            break;
+                    }
+                }
+            }
         }
         
         if (rangeIndicator != null) rangeIndicator.SetActive(false);
